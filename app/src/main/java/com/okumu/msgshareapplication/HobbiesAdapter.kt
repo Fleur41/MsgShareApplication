0b8102171882457ba180aacefff34e837bc7ccc5
Fleur41 <Muriithikings456@gmail.com>
2024-05-22 15:52:01 +0300

    Update my init block with the currentHobby setOnClickListener on imgShare

--- app/src/main/java/com/okumu/msgshareapplication/HobbiesAdapter.kt
+++ app/src/main/java/com/okumu/msgshareapplication/HobbiesAdapter.kt
@@ -1,9 +1,12 @@
 package com.okumu.msgshareapplication
 
 import android.content.Context
+import android.content.Intent
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.okumu.msgshareapplication.databinding.ListItemBinding
 
@@ -25,9 +28,33 @@ class HobbiesAdapter(val context: Context, private val hobbies: List<Hobby>) : R
     }
 
     inner class MyViewHolder(private val binding: ListItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        var currentHobby: Hobby? = null
+//        var currentPosition: Int = 0
+        init {
+            binding.root.setOnClickListener{
+                currentHobby?.let {
+                    Toast.makeText(context, currentHobby!!.title + " Clicked !", Toast.LENGTH_SHORT).show()
+                }
+
+                binding.imgShare.setOnClickListener{
+                    currentHobby?.let {
+                        val message: String = "My hobby is: " + currentHobby!!.title
+                        val intent = Intent()
+                        intent.action = Intent.ACTION_SEND
+                        intent.putExtra(Intent.EXTRA_TEXT, message)
+                        intent.type = "text/plain"
+
+                        context.startActivity(Intent.createChooser(intent, "Share to: "))
+                    }
+                }
+            }
+        }
         fun setData(hobby: Hobby?) {
             binding.txvTitle.text = hobby?.title ?: ""
             Log.d("MyViewHolder", "setData: hobby = ${hobby?.title}")
+            this.currentHobby = hobby
+//            this.currentPosition = pos
         }
     }
 }
